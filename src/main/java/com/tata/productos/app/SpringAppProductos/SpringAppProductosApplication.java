d7ad47ffaa2f9834dbc45ac77a595c67fe5df862
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2020-04-09 03:52:53 -0400

    Principal Archive Empty
    Archive for start the project, that build the app, but is empty of configurations and business rules.

---
+++ src/main/java/com/tata/productos/app/SpringAppProductos/SpringAppProductosApplication.java
@@ -0,0 +1,13 @@
+package com.tata.productos.app.SpringAppProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tata/productos/app/SpringAppProductos/SpringAppProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tata.productos.app.SpringAppProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
