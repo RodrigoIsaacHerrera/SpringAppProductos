bf64fc9d1120453556fdf466447ca2771d1caff0
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2020-04-09 16:02:09 -0400

    Rutas de URL REST
    add the defferents mapping for the request by user.

--- src/main/java/com/tata/productos/app/SpringAppProductos/web/RestControlador.java
+++ src/main/java/com/tata/productos/app/SpringAppProductos/web/RestControlador.java
@@ -1,13 +1,37 @@
 package com.tata.productos.app.SpringAppProductos.web;
 
+import com.tata.productos.app.SpringAppProductos.core.model.ProductoDTO;
 import com.tata.productos.app.SpringAppProductos.core.model.ProductoRepositoryDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RestControlador {
     @Autowired
     ProductoRepositoryDAO dao;
+
+    @GetMapping("/Productos")
+    public List<ProductoDTO> getListaProductos(){
+
+        return this.dao.findAll();
+
+    }
+    @PostMapping("/Productos/RegistrarProducto")
+    public boolean addProducto(@RequestBody ProductoDTO nuevo){
+        if(!this.dao.existsById(nuevo.getId())) {
+            this.dao.save(nuevo);
+            return true;
+        }
+        return false;
+    }
+    @DeleteMapping("/EliminarProducto/{id}")
+    public boolean deleteProducto(@PathVariable Integer id){
+        this.dao.deleteById(id);
+        return !this.dao.existsById(id);
+    }
+
 
 
 }
